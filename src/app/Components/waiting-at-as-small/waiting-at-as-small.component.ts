23c651863172ed8c845a053373e88cf66f28c720
Duc Thang Duong <thangduong1611@gmail.com>
2020-05-29 08:03:24 +0200

    change Timer-Design

--- src/app/Components/waiting-at-as-small/waiting-at-as-small.component.ts
+++ src/app/Components/waiting-at-as-small/waiting-at-as-small.component.ts
@@ -6,13 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./waiting-at-as-small.component.scss'],
 })
 export class WaitingAtAsSmallComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit() {}
-
-  showMore(){
-  	console.log('hi!')
+  	
+  	showMore(){
+	  	var changeDivHeight = document.getElementById("myOver");
+	 	var x = document.getElementById("hide");
+	  	var y = document.getElementById("btn2-toChange");
+	  	changeDivHeight.style.height = "200px";
+	  	x.hidden=true;
+	  	y.style.height="80px";
+	  	y.style.marginTop="30px";
+	  	var z = document.getElementById("show-div");
+	  	var t = document.getElementById("hide-div");
+	  	z.hidden = true; 
+	  	t.hidden = false;
+  }
+  	hideMore(){
+	  	var changeDivHeight = document.getElementById("myOver");
+	  	var x = document.getElementById("hide");
+	  	var y = document.getElementById("btn2-toChange");
+	  	changeDivHeight.style.height = "auto";
+	  	y.style.height="30px";
+	  	y.style.marginTop="0px";
+	  	x.hidden=false;
+	  	var z = document.getElementById("show-div");
+	  	var t = document.getElementById("hide-div");
+	  	z.hidden = false;
+	  	t.hidden = true;
   }
 
 }
